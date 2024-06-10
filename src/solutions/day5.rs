3cce315345e8346df05103351f880d7c943ffd77
Martin Gabel <martin.gabel@becker-antriebe.com>
2024-06-10 13:43:16 +0200

    day5 part2

--- src/solutions/day5.rs
+++ src/solutions/day5.rs
@@ -9,6 +9,19 @@ fn string_is_nice(line: &str) -> bool {
         && line.chars().tuple_windows().any(|(a, b)| a == b)
 }
 
+fn string_is_nice_v2(line: &str) -> bool {
+    let has_aba = line
+        .chars()
+        .tuple_windows::<(_, _, _)>()
+        .any(|(a, b, c)| a == c && a != b);
+    let has_pair = line
+        .chars()
+        .tuple_windows::<(char, char)>()
+        .enumerate()
+        .any(|(i, a)| line.chars().skip(i + 2).tuple_windows().any(|b| a == b));
+    return has_aba && has_pair;
+}
+
 impl solutions::Solver for Solution {
     fn part1(&self, input: &str) -> String {
         let mut amount = 0;
@@ -19,8 +32,15 @@ impl solutions::Solver for Solution {
         }
         return format!("{}", amount);
     }
-    fn part2(&self, _input: &str) -> String {
-        return format!("2");
+
+    fn part2(&self, input: &str) -> String {
+        let mut amount = 0;
+        for line in input.lines() {
+            if string_is_nice_v2(line) {
+                amount += 1;
+            }
+        }
+        return format!("{}", amount);
     }
 }
 
@@ -44,6 +64,10 @@ mod tests {
     fn test_part2() {
         let s = Solution;
         let solver: &dyn Solver = &s;
-        assert_eq!(solver.part2(""), "2");
+        assert_eq!(solver.part2("aaa"), "0");
+        assert_eq!(solver.part2("qjhvhtzxzqqjkmpb"), "1");
+        assert_eq!(solver.part2("xxyxx"), "1");
+        assert_eq!(solver.part2("uurcxstgmygtbstg"), "0");
+        assert_eq!(solver.part2("ieodomkazucvgmuy"), "0");
     }
 }
