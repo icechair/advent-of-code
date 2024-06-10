9cf07dc69349a28bd98094e0b2d90df9ef67fcdc
Martin Gabel <martin.gabel@becker-antriebe.com>
2024-06-10 09:41:52 +0200

    day1

---
+++ src/solutions/day1.rs
@@ -0,0 +1,61 @@
+use crate::solutions;
+pub struct Solution;
+
+impl solutions::Solver for Solution {
+    fn part1(&self, input: &str) -> String {
+        let mut floor = 0;
+        for c in input.chars() {
+            match c {
+                '(' => floor += 1,
+                ')' => floor -= 1,
+                _ => unreachable!(),
+            }
+        }
+        return format!("{}", floor);
+    }
+    fn part2(&self, input: &str) -> String {
+        let mut floor = 0;
+        let mut n = 0;
+        for c in input.chars() {
+            n += 1;
+            match c {
+                '(' => floor += 1,
+                ')' => floor -= 1,
+                _ => unreachable!(),
+            }
+            if floor < 0 {
+                return format!("{}", n);
+            }
+        }
+        return format!("2");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::solutions::Solver;
+
+    use super::*;
+    #[test]
+    fn test_part1() {
+        let s = Solution;
+        let solver: &dyn Solver = &s;
+        assert_eq!(solver.part1("(())"), "0");
+        assert_eq!(solver.part1("()()"), "0");
+        assert_eq!(solver.part1("((("), "3");
+        assert_eq!(solver.part1("(()(()("), "3");
+        assert_eq!(solver.part1("))((((("), "3"); // also results in floor 3.
+        assert_eq!(solver.part1("())"), "-1");
+        assert_eq!(solver.part1("))("), "-1"); // both result in floor -1 (the first basement level).
+        assert_eq!(solver.part1(")))"), "-3");
+        assert_eq!(solver.part1(")())())"), "-3"); // both result in floor -3.
+    }
+
+    #[test]
+    fn test_part2() {
+        let s = Solution;
+        let solver: &dyn Solver = &s;
+        assert_eq!(solver.part2(")"), "1");
+        assert_eq!(solver.part2("()())"), "5");
+    }
+}
